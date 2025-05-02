61665827ffe7875cbbb69bf5fb5270dd4f329aa6
Pritom Rahaman <pritom@Pritoms-MacBook-Pro.local>
2025-05-02 09:42:09 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Pritom Rahaman',
+    author_email= 'pritomrahaman1331@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
